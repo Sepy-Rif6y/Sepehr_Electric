aaf8b96702a2aa2bac418ce0e8e7eb72235b6191
Sepehr_GH <119931395+Sepy-Rif6y@users.noreply.github.com>
2025-08-25 00:48:11 +0330

    editing about-us page

--- resources/views/Site_Template/Main_Contents/About_Us.blade.php
+++ resources/views/Site_Template/Main_Contents/About_Us.blade.php
@@ -7,16 +7,9 @@
 @endsection
 {{-- جایگذاری در ییلد --}}
 @section("MainContent")
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body style="width: 100%">
-    <div>Coming soon</div>
-</body>
-</html>
+<div class="col-lg-8 col-md-12" style="text-align: center">
+    <div class="AboutUsStyle">
+        Coming Soon
+    </div>
+</div>
 @endsection
